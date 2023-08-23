1a627a602ad89e7253e267700ac82a1bfc75deaa
Your Name <you@example.com>
2023-08-23 22:00:47 +0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# GitHub Pages
+
+
+
+# appveyor
+
+[![Build status](https://ci.appveyor.com/api/projects/status/2xaqbrv16wa7qldy?svg=true)](https://ci.appveyor.com/project/Volivanmail/anime-collaps)
+
+
+
+
+
---
+++ src/js/collapse.js
@@ -0,0 +1,16 @@
+export default class Collapse {
+  createCollapse() {
+    const btn = document.querySelector(".btn");
+    const borderText = document.querySelector(".border-text");
+
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      if (borderText.classList.contains("show")) {
+        borderText.classList.remove("show");
+        return;
+      }
+      borderText.classList.add("show");
+    });
+  }
+}
